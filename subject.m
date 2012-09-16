c050db7513d8c732cf53845a0da1fa5cd9121193
Lukasz Kolodziejczyk <lkolodziejczyk89@gmail.com>
2012-09-16 13:03:37 +0200

    osobnik

--- subject.m
+++ subject.m
@@ -1,12 +1,23 @@
 classdef subject
     properties
         time;
-        A;
+        mainArray; %tablica: index | x | y | z | trasa 5x250
+        palletsArray; %tablica: x | y | z
         
     end
     methods
         function obj = subject(A)
-            obj.A = A;
+            obj.palletsArray = A;
+            array = zeros(250,5); 
+            [rowNum,columnNum]=size(A);
+        
+            for rows=1:1:rowNum
+                array(rows,1) = rows; %set wood index 1 - 250
+                for columns=1:1:columnNum
+                   array(rows,1+columns) = A(rows,columns); %set x y z
+                end
+            end
+            obj.mainArray = array;
         end
 
         function obj = setEndFunction(time)
@@ -14,7 +25,7 @@ classdef subject
         end
         
         function obj = setRoad(index,road)
-            obj.A(index,4) = road;
+            obj.mainArray(index,4) = road;
         end
     end
 end
