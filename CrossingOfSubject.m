fe6127e307359cb9c58b0424c608ed672efc8ee6
Iwona Pedrys <iwonampedrys@wp.pl>
2012-09-16 18:45:00 +0200

    krossing

--- CrossingOfSubject.m
+++ CrossingOfSubject.m
@@ -1,28 +1,29 @@
 function object=CrossingOfSubject(do_mutowania,Palety,M)
    
      
-    [rowsNum, columnNum] = size(do_mutowania(1));
+    [rowsNum, columnNum] = size(do_mutowania());
     A = zeros(Palety,3);
     miejsce = rand(1,1);
     miejsce = round(miejsce*Palety);
     pierwszy = rand(1,1);
-    pierwszy = round(miejsce*M);
+    pierwszy = round(pierwszy*M);
     drugi = rand(1,1);
-    drugi = round(miejsce*M);
-    while pierwszy ~= drugi 
+    drugi = round(drugi*M);
+    while pierwszy == drugi 
         drugi = rand(1,1);
-        drugi = round(miejsce*M);
+        drugi = round(drugi*M);
     end;
+
         
     for rows=1:1:miejsce
-            A(rows,1) = do_mutowania(pierwszy(rows,1),1);
-            A(rows,2) = do_mutowania(pierwszy(rows,2),1);
-            A(rows,3) = do_mutowania(pierwszy(rows,3),1);
+            A(rows,1) = do_mutowania(1,pierwszy(rows,1));
+            A(rows,2) = do_mutowania(1,pierwszy(rows,2));
+            A(rows,3) = do_mutowania(1,pierwszy(rows,3));
     end;
-       for rows=miejsce:1:rowsNum
-           A(rows,1) = do_mutowania(drugi(rows,1),1);
-           A(rows,2) = do_mutowania(pierwszy(rows,2),1);
-           A(rows,3) = do_mutowania(pierwszy(rows,3),1);
+       for rows=miejsce:1:Palety
+           A(rows,1) = do_mutowania(1,drugi(rows,1));
+           A(rows,2) = do_mutowania(1,drugi(rows,2));
+           A(rows,3) = do_mutowania(1,drugi(rows,3));
        end;
         
     obj.newMainArray(A);
