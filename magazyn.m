29ef83be287d70adab720b37c0cf5c21be2e031b
jasiek <yumyoggy@gmail.com>
2012-09-16 13:51:50 +0200

    hack dla magazynu zeby nie dalo sie za malego stworzyc
    poczatek wypelniania przejazdu

--- magazyn.m
+++ magazyn.m
@@ -6,10 +6,10 @@ classdef magazyn
         % 1 to zajete miejsce
         % 8 to pusta sciezka
         % 7 to zajeta sciezka
-        heigth
-        width
-        length
-        map
+        heigth;
+        width;
+        length;
+        map;
         wozek_count = 4;
         paths_offsets = [2 10];
         wozek_start = [0 0];
@@ -21,6 +21,18 @@ classdef magazyn
         %@param length magazyn length
         %@param height magazyn heigth
         function mag = magazyn(width, length, heigth)
+            if width < 10
+                width = 10;
+                disp('width nie moze byc mniejsze niz 10');
+            end
+            if length < 4
+                length = 4;
+                disp('length nie moze byc mniejsze niz 4');
+            end
+            if heigth < 1
+                length = 1;
+                disp('heigth nie moze byc mniejsze niz 1');
+            end
             mag.width = width + width/mag.paths_offsets(2) + 1;
             mag.heigth = heigth;
             mag.length = length + length/mag.paths_offsets(1) + 1;
@@ -34,10 +46,12 @@ classdef magazyn
             %disp(mag.map);
         end % magazyn
         
+        % dispaly maazyn map
         function display_current_state(mag)
             disp(mag.map);
-        end
+        end % display_current_state
         
+        % prepare start state ofmagazyn map (there are some packs there already)
         function psp = prepare_start_packs(mag, limit)
             for i = 1:limit
                  
@@ -49,6 +63,6 @@ classdef magazyn
             end % for
             disp(mag.map);
             psp = mag;
-        end          
+        end % prepare start packs         
     end % methods
 end     
