f93acace31e10df4db5dfa7b9f8e18b29805cfd7
lkolodziejczyk <lkolodziejczyk89@gmail.com>
2012-09-17 21:54:23 +0200

    f

--- main.m
+++ main.m
@@ -66,7 +66,7 @@ end;
 %%%%%%%%%%%%%Krzy¿owanie
 		poczatek2=poczatek+round(M*0.01)+1; %zaczynmy w nastêpnym po zmutowanych, koñczymy po K% kolejnych-> czyli na koñcu
         for ile=poczatek2:1:P
-			%tablicaPopulacji(ile)=CrossingOfSubject(mutowanie,Palety,M,P);
+			tablicaPopulacji(ile)=CrossingOfSubject(mutowanie,Palety,M,P);
         end;
 %        for ile=poczatek2:1:P
 %			tablicaPopulacji(ile).palletsArray=CrossingOfSubject(mutowanie,Palety,M,P);
@@ -76,45 +76,54 @@ end;
         %tablicaPopulacji=deploy(obiektMagazyn,tablicaPopulacji);
 		%w wyniku do otrzymujemy tablice osobników po przejœciu przezmagazyn wraz z
 		%wartoœci¹ funkcji celu
-        %for b=1:1:P
-        %tablicaPopulacji(b).time=(rand(1,1)*100);
-        %end;
+        for b=1:1:P
+        tablicaPopulacji(b).time=(rand(1,1)*100);
+        end;
 %%%%%%%%%%%%%%SORTOWANIE TABLICY OSOBNIKOW
 	%populacja[][]=sortuj_populacje(populacja[][]); %sortowanie populacji
 	%(osobnikow) wg wartoœci funkcji celu: najlepsze (o najmniejszej
 	%wartoœci funkcji celu wy¿ej)
-    %for b=1:1:P
-    %wartosci(b,1)=b;
-    %wartosci(b,2)=tablicaPopulacji(b).time;
-    %end;
-    %[val, ind] = sort(wartosci(:, 2));    % col - numer kolumny
-    %wartosci = wartosci(ind, :);
-    %srednia=mean(wartosci(:,2));
-    %for b=1:1:P
-    %tablicaPomoc(b)=tablicaPopulacji(wartosci(b,1));
-    %end;
-    %for b=1:1:P
-    %tablicaPopulacji(b)=tablicaPomoc(b);
-    %end;
+    for b=1:1:P
+    wartosci(b,1)=b;
+    wartosci(b,2)=tablicaPopulacji(b).time;
+    end;
+    [val, ind] = sort(wartosci(:, 2));    % col - numer kolumny
+    wartosci = wartosci(ind, :);
+    srednia=mean(wartosci(:,2));
+    for b=1:1:P
+    tablicaPomoc(b)=tablicaPopulacji(wartosci(b,1));
+    end;
+    for b=1:1:P
+    tablicaPopulacji(b)=tablicaPomoc(b);
+    end;
 	
 	
 %%%%%%%%5%ZAPYSYWANIE WYNiKOW.... (?) dla ka¿dej pêtli: min,œr i max
 %%%%%%%%wartoœæ funkcji celu 
-   % minimum=tablicaPopulacji(1).time;
-   % maximum=tablicaPopulacji(P).time;
+    minimum=tablicaPopulacji(1).time;
+    maximum=tablicaPopulacji(P).time;
    % minimum
    % maximum
    % srednia
 	%jakas_funkcja_zapisujaca_jakos_wyniki;
-        %W_min(i)=minimum;
-        %W_max(i)=maximum;
-        %W_mean(i)=srednia;
+       W_min(i)=minimum;
+       W_max(i)=maximum;
+       W_mean(i)=srednia;
     i    
     end;    
 
 %%%%%%%%%%% wyœwietlanie wykresu z wczeœniej zapisanych wartoœci min,œr i
 %%%%%%%%%%% max
-    %plot(odczegoœtam);
-
-   
+   figure(1); plot(1:1:ILE,W_min);
+    xlabel('ilosc iteracji');
+    ylabel('Funkcja czasu');
+    title('Minimum');
+   figure(2); plot(1:1:ILE,W_max);
+    xlabel('ilosc iteracji');
+    ylabel('Funkcja czasu');
+    title('Maksimum');
+   figure(3); plot(1:1:ILE,W_mean);
+    xlabel('ilosc iteracji');
+    ylabel('Funkcja czasu');
+    title('Œrednia');
 end
